f8fc19e8250eada2d8e2aa94fe0affec1ae8a025
ozzyOwl <ozzyowl@20201101-Macmini-2018.local>
2021-01-03 18:43:45 +0800

    reorder the code

--- all_test.py
+++ all_test.py
@@ -1,13 +1,14 @@
 import akshare as ak
 import pandas as pd
 
-fundlist = ['164902','519723','519782','519718','006793','519755','519738','519752','002503','002058','002435','003967','002414','519772','519773','519704','000478','100032','163407','110020','001469','001064','001052','004752','100038','001180','002708','000968','502010','161017','002903','000051','110027','340001','270048','000563','000147','003376','002001','000071','000216']
+fundlist = ['164902','519723','519782','519718','006793','519755','519738','519752','002503','002058','002435','003967','002414','519772','519773','519704','163407','110020','100038','000051','000478','001052','161017','002903','100032','001180','002708','000968','001064','004752','001469','502010','110027','340001','270048','000563','000147','003376','002001','000071','000216']
 overseefundlist = ['000614','050025','164906','162411','160416']
-etflist =['162411','159920','159938','512980'] # 162411 no data
-a_stockList = ['600036','000002']
-us_stockList = ['TLT','IEF','SGOL','DBC'] # VOO no data
+etflist =['159938','512980','159920']
 sz162411_code = 'sz162411'
+a_stockList = ['600036','000002']
 voo_code = 'VOO'
+us_stockList = ['TLT','IEF','SGOL','DBC'] # VOO no data
+
 
 def select_today_price(df,codeColumn,valueColumn,codelist):
   df = df[df[codeColumn].isin(codelist)] #filter selected code
@@ -46,6 +47,6 @@ voo_df = select_today_close_price(voo_df,voo_code)
 
 us_stock_df = select_today_price(ak.stock_us_spot(),'symbol','price',us_stockList)
 
-#toCSV
+#toCS
 open_df.append(qdii_df).append(etf_df).append(sz162411_df).append(a_stock_df).append(voo_df).append(us_stock_df).to_csv('all_test.csv')
 
